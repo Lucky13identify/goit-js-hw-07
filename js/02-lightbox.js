0be12ec28f858eb0ae7c81ba7ea8b8b69d2fe42f
Lucky13identify <108291368+Lucky13identify@users.noreply.github.com>
2023-01-31 17:49:51 +0200

    new

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -8,9 +8,18 @@ import { galleryItems } from "./gallery-items.js";
 const containerGallery = document.querySelector(".gallery");
 const photosCards = createMarkup(galleryItems);
 containerGallery.insertAdjacentHTML("afterbegin", photosCards);
-containerGallery.addEventListener("click", onCardImgClickTarget);
+// containerGallery.addEventListener("click", onCardImgClickTarget);
 
 // Функция перебирает масив и создает новый масив элементов + содает разметку
+
+var lightbox = new SimpleLightbox(".gallery a", {
+  captions: true,
+  captionSelector: "img",
+  captionType: "attr",
+  captionsData: "alt",
+  captionPosition: "bottom",
+  captionDelay: 250,
+});
 
 function createMarkup(galleryItems) {
   return galleryItems
@@ -22,10 +31,10 @@ function createMarkup(galleryItems) {
     .join("");
 }
 
-function onCardImgClickTarget(evt) {
-  evt.preventDefault();
-  if (!evt.target.classList.contains("gallery__image")) {
-    return;
-  }
-  var lightbox = new SimpleLightbox(".gallery a", { captionDelay: 250 });
-}
+// function onCardImgClickTarget(evt) {
+//   evt.preventDefault();
+//   if (!evt.target.classList.contains("gallery__image")) {
+//     return;
+//   }
+//   console.log("sss");
+// }
